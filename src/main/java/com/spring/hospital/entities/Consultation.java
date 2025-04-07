eea48b64c684e3a54986c3780fc51cae2d0512f2
MoBourhym <simobourhym01@gmail.com>
2025-04-07 22:41:45 +0100

    Entities creation

---
+++ src/main/java/com/spring/hospital/entities/Consultation.java
@@ -0,0 +1,24 @@
+package com.spring.hospital.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Consultation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    private String rapport;
+    @OneToOne
+    private RendezVous rendezVous;
+}
---
+++ src/main/java/com/spring/hospital/entities/StatusRDV.java
@@ -0,0 +1,9 @@
+package com.spring.hospital.entities;
+
+public enum StatusRDV {
+    PENDING,
+    CONFIRMED,
+    CANCELLED,
+    REJECTED,
+    DONE
+}
